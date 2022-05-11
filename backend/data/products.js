19eeb93c18e3df4926cdf803bb2088ee7501a47c
Mario Colunga <marioco71@gmail.com>
2022-05-10 23:07:16 -0500

    Final Project

---
+++ backend/data/products.js
@@ -0,0 +1,112 @@
+const products = [
+  {
+    name: 'Full-Spectrum',
+    image: '/images/aceitemedicinal.jpg',
+    description:
+      'medicinal oil, lemon fragance 30ml',
+    brand: 'Full Spectrum',
+    category: 'Oils',
+    price: 500,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: 'CBD Extra Ratio 1:1',
+    image: '/images/CBDExtraRatio1.jpg',
+    description:
+      'Ideal oil to minimize severe physical discomfort.',
+    brand: 'Nanko',
+    category: 'Oil',
+    price: 900,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    name: 'SWEED CBD Green',
+    image: '/images/SWEEDCBDGreenAutimo.jpg',
+    description:
+      'Ideal for children and adults who suffer from some type of epilepsy, it helps reduce the number and intensity of seizures, promotes rest, stimulates cognitive development and improves appetite. In children and young people with autism it helps to control tics, depression, restlessness and rage attacks. It is recommended for conditions such as epilepsy, autism, anxiety, addictions, West syndrome, ADD, Asperger syndrome, depression, among others',
+    brand: 'Full Spectrum',
+    category: 'CBD',
+    price: 1200,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    name: 'Gummies',
+    image: '/images/Gomitas.jpg',
+    description:
+      'excellent option for anxiety, pain, attention deficit, insomnia among others. Combines the full-spectrum benefits of hemp and its derivatives with great taste. Because they are rationed, they are ideal for dosing. Bottle with 60 HEMP gummies (20 mg each).',
+    brand: 'Raw',
+    category: 'Edibles',
+    price: 200,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    name: 'Ointment CBD regenerator',
+    image: '/images/PomadaRegeneradoraCBD.jpg',
+    description:
+      'To treat diseases such as psoriasis, herpes, acne, dermatitis, burns, chafing, stretch marks or cosmetic use.',
+    brand: 'Nanko',
+    category: 'Oinments',
+    price: 250,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 11,
+  },
+  {
+    name: 'analgesic ointment chronic pain',
+    image: '/images/PomadaDolorCrónico.jpg',
+    description:
+      'Relieves chronic degenerative pain caused by rheumatic diseases',
+    brand: 'Nanko',
+    category: 'Oinments',
+    price: 350,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+  {
+    name: 'Ointment CBD regenerator',
+    image: '/images/CBDgummies.jpg',
+    description:
+      'CBD Gummies are a form of CBD edibles that delivers cannabidiol in the very familiar shape of gummy bears. This type of CBD edibles is chosen by people who want to naturally reduce anxiety, depression, and pain, and live a calmer life with less stress and inflammation, and more energy.',
+    brand: 'Author Ferdinand H. Quinones',
+    category: 'Books',
+    price: 306,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 11,
+  },
+  {
+    name: 'California: Black Tea Flavor Carbonated Drink with CBD California Can 473 ml',
+    image: '/images/californiaDrink.jpg',
+    description:'Refreshing carbonated drink with cannabidiol natural extracts and flavors, naturally gluten free, THC-free, ideal for all lifestyles.',
+    brand: 'Author Ferdinand H. Quinones',
+    category: 'Drinks',
+    price: 38,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 11,
+  },
+  {
+    name: 'Facecalm CBD Soothing Mask',
+    image: '/images/FacecalmCBD.jpg',
+    description:'CBD + 2% Niacinamide Soothing Mask A creamy soothing mask made for all skin types to help deeply hydrate skin.',
+    brand: 'JUNO & Co.',
+    category: 'Oinment',
+    price: 38,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 11,
+  }
+
+
+];
+
+export default products;
